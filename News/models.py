4705671e984b8846d687544f30409c2e1a98d67d
noelia.lopez.cano <noelia.lopez.cano@estudiantat.upc.edu>
2024-11-03 22:46:35 +0100

    task #43: Create comments

--- News/models.py
+++ News/models.py
@@ -38,6 +38,7 @@ class Comments(models.Model):
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     published_date = models.DateTimeField(auto_now=True)
     New = models.ForeignKey(News, on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')
 
 class Search(models.Model):
     text = models.CharField(max_length=1000)
--- News/urls.py
+++ News/urls.py
@@ -16,7 +16,6 @@ urlpatterns = [
     path('submit/', views.submit, name='submit_news'),
     path('newest/', NewestListView.as_view(), name='newest_news'),
     path('ask/', AskListView.as_view(), name='ask_list'),
-    path('ask/<int:ask_id>/', views.ask_detail, name='ask_detail'),
     path('login/', views.login, name='login'),
     path('user/', UserView.as_view(), name='user_profile'),
     path('logout/', views.logout, name='logout'),
@@ -24,5 +23,6 @@ urlpatterns = [
     path('search/', SearchListView.as_view(), name='search_list'),
     path('edit/<int:news_id>/', views.edit_news, name='edit_news'),
     path('delete/<int:news_id>/', views.delete_news, name='delete_news'),
+    path('item/<int:news_id>/', views.item_detail, name='item_detail'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
