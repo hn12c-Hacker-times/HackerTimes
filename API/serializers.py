632038675ff17844a08f5fe965825280dc998fe6
enrique.andujar <enrique.andujar@estudiantat.upc.edu>
2024-11-18 13:51:22 +0100

    Creació de l'API i el primer endpoint acabat!

---
+++ API/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from News.models import News, Comments, CustomUser, HiddenNews, Thread
+
+class NewsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = News
+        fields = [ 'title', 'url', 'urlDomain', 'text', 'author', 'published_date', 'points', 'is_hidden']
+
+class CommentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comments
+        fields = [ 'text', 'author', 'published_date', 'New', 'parent']
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = [ 'username','email', 'created_at', 'karma', 'about', 'banner', 'avatar', 'show_dead', 'no_procrastinate', 'max_visit', 'min_away', 'delay', 'favorite_news', 'favorite_comments']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+class HiddenNewsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HiddenNews
+        fields = [ 'user', 'news', 'hidden_at']
+
+        
+class ThreadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Thread
+        fields = [ 'title', 'comments', 'updated_at']
